e1cb2e7b389a5891e8a6927ef5e521312db00e4e
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-03-27 04:30:07 -0500

    started resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,14 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You must be logged in!");
+    },
+  },
+};
