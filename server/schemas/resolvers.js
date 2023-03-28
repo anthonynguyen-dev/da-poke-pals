8e868ea64f73ec9eb6113081a3914d35e279dbcd
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-03-28 18:17:34 -0500

    added pokemon and index models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,6 +13,7 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
+      console.log(username, email, password);
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,8 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email: String
     password: String
     pokeDex: [Pokemon]
@@ -15,9 +15,9 @@ const typeDefs = gql`
     description: String
     pokemonId: ID
     image: String
-    moves: String
-    abilites: String
-    evolutions: String
+    moves: [String]
+    abilites: [String]
+    evolutions: [String]
   }
   type Auth {
     token: ID!
