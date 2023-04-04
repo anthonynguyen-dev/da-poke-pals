2c1e91e6a04b522575dc1856dd1c61bd4b47424f
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-04-03 21:10:33 -0500

    fixed pokemon sprite

--- client/src/containers/pokemonContainer.js
+++ client/src/containers/pokemonContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from "@apollo/react-hooks";
 import { GET_POKEMONS, POKEMON_DETAILS } from "../utils/queries";
 
 import { Pokemon } from "../containers/pokemon";
@@ -12,28 +12,36 @@ const gVariables = {
   offset: 1,
 };
 
+export function PokemonContainer({ pokeSearch }) {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: gVariables,
+    context: { clientName: "pokemonApi" },
+  });
 
+  const { data } = useQuery(POKEMON_DETAILS, {
+    variables: {
+      name: pokeSearch,
+    },
+    context: { clientName: "pokemonApi" },
+  });
 
+  console.log("pokemon");
+  console.log(data);
 
-export function PokemonContainer({pokeSearch}) {
-    const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-        variables: gVariables,
-    });
-
-    const { data } = useQuery(POKEMON_DETAILS, {
-        variables: {
-            name:pokeSearch
-        },
-    });
-
-
-
-    return (
-        <div className='pokemons' >
-            
-            {pokeSearch!=="" ?  data && data.pokemon &&  <Pokemon pokemon={data.pokemon} key={data.pokemon.name} />  :
-            pokemons && pokemons.results && pokemons.results.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name} />  )}
-        </div>
-    )
+  const pokemonData = {
+    name: data.pokemon.name,
+    image: data.pokemon.sprites.front_default,
+  };
+  return (
+    <div className="pokemons">
+      {pokeSearch !== ""
+        ? data &&
+          data.pokemon && <Pokemon pokemon={pokemonData} key={pokemonData} />
+        : pokemons &&
+          pokemons.results &&
+          pokemons.results.map((pokemon) => (
+            <Pokemon pokemon={pokemon} key={pokemon.name} />
+          ))}
+    </div>
+  );
 }
-
