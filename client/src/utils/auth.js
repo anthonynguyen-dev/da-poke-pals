b64777d83b61bb49376758c66ea65b56b0fd35f5
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-04-03 20:30:57 -0500

    fixing everything wrong with our application

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -9,3 +9,14 @@
 //     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
 //   },
 // };
+
+import decode from "jwt-decode";
+
+class AuthService {
+  login(idToken) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+}
+
+export default new AuthService();
